99faafff591f20d7866305bbd0d3198e3590b047
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-07-18 15:06:14 +0900

    Post 모델 category 필드 변경

--- blog/models.py
+++ blog/models.py
@@ -6,15 +6,11 @@ User = get_user_model()
 # Create your models here.
 class Post(models.Model):
     
-    CATEGORY_1 = "T1"
-    CATEGORY_2 = "T2"
-    CATEGORY_CHOICES = ((CATEGORY_1, "Test1"), (CATEGORY_2, "Test2"))
-    
     title = models.CharField(max_length=30)
     content = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(default='active', max_length=20)
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2, null=True, blank=True)
+    category = models.CharField(max_length=30)
     views = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
